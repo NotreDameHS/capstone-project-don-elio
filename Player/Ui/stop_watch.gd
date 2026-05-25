8fc5527c0aa49d9881d76c4c1cf571ceacc3dd9d
donionUh <donn1@learn.cssd.ab.ca>
2026-05-25 11:44:09 -0600

    timer

--- Player/Ui/stop_watch.gd
+++ Player/Ui/stop_watch.gd
@@ -1,6 +1,6 @@
 extends Node2D
-
 class_name  StopWatch
+
 var time = 0.0
 var stopped = false
 
@@ -11,3 +11,12 @@ func _process(delta: float) -> void:
 	
 func reset():
 	time = 0.0
+
+
+func time_to_string() -> String:
+	var msec = fmod(time, 1) * 1000
+	var sec =  fmod(time, 60)
+	var min = time / 60
+	var format_string = "%02d : %02d : %02d"
+	var acutal_string = format_string % [min, sec, msec]
+	return acutal_string
--- Player/Ui/ui.gd
+++ Player/Ui/ui.gd
@@ -1,12 +1,21 @@
-extends CanvasLayer
+extends Node
+class_name Ui
 
-# Called when the node enters the scene tree for the first time.
+@export var stopwatch_label : Label
+
+var stopwatch : StopWatch
+
 func _ready() -> void:
-	pass # Replace with function body.
+	stopwatch = get_tree().get_first_node_in_group("stopwatch")
+
+func update_stopwatch_label():
+	stopwatch_label.text = stopwatch.time_to_string()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	update_stopwatch_label()
 	pass
 
 
