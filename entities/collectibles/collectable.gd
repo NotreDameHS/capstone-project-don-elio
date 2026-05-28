e94763c1331d3776871a4ff83940920ebded3499
donionUh <donn1@learn.cssd.ab.ca>
2026-05-28 13:34:25 -0600

    Ui Adjustments

--- entities/collectibles/collectable.gd
+++ entities/collectibles/collectable.gd
@@ -1,6 +1,6 @@
 extends Area2D
 class_name Collectable
-
+@export var exp_amount = 10
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,5 +11,10 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_area_entered(area: Area2D) -> void:
-	queue_free()
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.has_method("gain_exp"):
+		print("Expereience gained :" + str(exp_amount))
+		body.gain_exp(exp_amount)
+		queue_free()
+	pass # Replace with function body.
