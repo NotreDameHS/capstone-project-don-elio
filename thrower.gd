dffad82c3cb4cad50b37c3435f6ca390cfc63094
Eliodlcp <eliodlcp@gmail.com>
2026-06-03 14:14:40 -0600

    remove thrower

--- thrower.gd
+++ thrower.gd
@@ -13,5 +13,8 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	var player := get_tree().get_nodes_in_group("Player")[0]
-	if distance_to(get_nodes_in_group("Player")) >
+	if get_tree().current_scene.distance_to(player) > 300:
+		speed *= -1
+	else:
+		speed *= 1
 	position = position.move_toward(player.global_position, speed * delta)
